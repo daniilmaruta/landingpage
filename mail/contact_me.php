658c7a07b1f2608ffa284fc92412536e2706c1f8
Sebastian <kreativmonkey@freifunk-myk.de>
2015-09-03 23:18:07 +0200

    E-Mail Funktion erweitert

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,10 +15,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'webmaster@mta-r.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "[FFMYK] Kontaktanfrage von  $name";
-$email_body = "Eine neue Nachricht wurde über das Kontaktformular versendet.\n\n"."Hier die Details:\n\nName: $name\n\nE-Mail: $email_address\n\nTelefon: $phone\n\nNachricht:\n$message";
-$headers = "From: noreply@freifunk-myk.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'mayen-koblenz@freifunk.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Kontaktanfrage von  $name";
+$email_body = "Eine neue Nachricht wurde über das Kontaktformular versendet.\n Bitte antworte in Kopie an die Mailinglist damit alle wissen das die Frage beantwortet wurde \n\n"."Hier die Details:\n\nName: $name\n\nE-Mail: $email_address\n\nTelefon: $phone\n\nNachricht:\n$message";
+$headers = "From: kontakt@freifunk-myk.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
